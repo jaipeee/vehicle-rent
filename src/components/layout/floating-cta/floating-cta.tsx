2e215f8c07c058a76aa88dcdc320d739769c25f7
jaipeee <jaipratapjaipee@gmail.com>
2026-09-22 22:58:14 +0530

    CTA button update

--- src/components/layout/floating-cta/floating-cta.tsx
+++ src/components/layout/floating-cta/floating-cta.tsx
@@ -25,7 +25,7 @@ const ACTIONS: CtaAction[] = [
 
 export function FloatingCta() {
   return (
-    <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end gap-3">
+    <div className="fixed bottom-1/2 right-6 z-50 flex flex-col items-end gap-3">
       {ACTIONS.map(({ label, href, icon: Icon, external }) => (
         <a
           key={label}
